35934510b72f798e54391bfeb78d3e8330b07ac7
Amit Gupta <iamitgupta1994@gmail.com>
2021-09-18 20:03:22 +0545

    [add][filter_using_filter_predicate]: filters using filter predicate, Higher Order Function

--- src/main/Main.kt
+++ src/main/Main.kt
@@ -1,23 +1,23 @@
 package main
 
 
-lateinit var companyList:MutableList<Company>
+lateinit var companyList: MutableList<Company>
 
-fun main(args:Array<String>){
+fun main(args: Array<String>) {
     println("Filter Sort Map Reduce in Kotlin")
 
     companyList = mutableListOf()
 
-    companyList.add(Company("C1","Technology",startYear = 1995,endYear = null))
-    companyList.add(Company("C2","Finance",startYear = 1994,endYear = 2011))
-    companyList.add(Company("C3","Industry",startYear = 1993,endYear = 2020))
-    companyList.add(Company("C4","Auto",startYear = 2001,endYear = 2021))
-    companyList.add(Company("C5","Hotel",startYear = 2001,endYear = 2020))
-    companyList.add(Company("C6","Technology",startYear = 2005,endYear = 2015))
-    companyList.add(Company("C7","Finance",startYear = 1996,endYear = 2014))
-    companyList.add(Company("C8","Finance",startYear = 1980,endYear = 2007))
-    companyList.add(Company("C9","Auto",startYear = 1985,endYear = 2008))
-    companyList.add(Company("C10","Hotel",startYear = 1989,endYear = 2004))
+    companyList.add(Company("C1", "Technology", startYear = 1995, endYear = null))
+    companyList.add(Company("C2", "Finance", startYear = 1994, endYear = 2011))
+    companyList.add(Company("C3", "Industry", startYear = 1993, endYear = 2020))
+    companyList.add(Company("C4", "Auto", startYear = 2001, endYear = 2021))
+    companyList.add(Company("C5", "Hotel", startYear = 2001, endYear = 2020))
+    companyList.add(Company("C6", "Technology", startYear = 2005, endYear = 2015))
+    companyList.add(Company("C7", "Finance", startYear = 1996, endYear = 2014))
+    companyList.add(Company("C8", "Finance", startYear = 1980, endYear = 2007))
+    companyList.add(Company("C9", "Auto", startYear = 1985, endYear = 2008))
+    companyList.add(Company("C10", "Hotel", startYear = 1989, endYear = 2004))
 
     // print Company List using For Loop
     println("------------------------------- printListUsingForLoop ---------------------------")
@@ -32,11 +32,14 @@ fun main(args:Array<String>){
      */
     // Filter Using Loop
     filterUsingLoop(companyList)
+
+    // Filter Using Filter Predicate
+    filterUsingFilterPredicate(companyList)
 }
 
 fun printListUsingForLoop(list: MutableList<Company>) {
 
-    for (i in 0 until list.size){
+    for (i in 0 until list.size) {
         println(list[i])
     }
     print("\n")
@@ -45,8 +48,13 @@ fun printListUsingForLoop(list: MutableList<Company>) {
 fun printListUsingForEachLoop(list: MutableList<Company>) {
 
     /**
-     * This approach is Passing One Function inside Another Function (called as Lambda Function)
-     * It can be used for writing more Readable and Concise code
+     * Approach :
+     * This approach is Passing One Function inside Another Function(i.e Higher Order Function in Kotlin) (called as Lambda Function)
+     * lambda functions are not declared but are passed immediately as an expression, So everything inside the {} is
+     * function definition, and this function will be called by the parent function (Note the params of Lambda Function are already defined by parent function)
+     *
+     * Usage:
+     *  It can be used for writing more Readable and Concise code
      */
     list.forEach {
         println(it)
@@ -61,7 +69,7 @@ fun filterUsingLoop(companyList: MutableList<Company>) {
 
     // Add the filtered data to New List
     companyList.forEach { company ->
-        if(company.type == "Technology")
+        if (company.type == "Technology")
             technologyCompanyList.add(company)
     }
 
@@ -74,7 +82,7 @@ fun filterUsingLoop(companyList: MutableList<Company>) {
 
     // Add the filtered data to New List
     companyList.forEach { company ->
-        if(company.startYear > 1994)
+        if (company.startYear > 1994)
             companyAfter1994List.add(company)
     }
 
@@ -82,4 +90,24 @@ fun filterUsingLoop(companyList: MutableList<Company>) {
     println("------------------------------- Filter Company List After 1994 Using Loop ---------------------------")
     printListUsingForEachLoop(companyAfter1994List)
 
+}
+
+fun filterUsingFilterPredicate(companyList: MutableList<Company>) {
+    // Q1. Filter List of Technology Companies
+    val technologyCompanyList = companyList.filter { company ->
+        company.type == "Technology"
+    }
+    // print the list
+    println("------------------------------- Filter Technology Company List Filter Predicate ---------------------------")
+    printListUsingForEachLoop(technologyCompanyList as MutableList<Company>)
+
+    // Q2. Filter List of Companies, which started after 1994 (i.e >1994)
+    // Add the filtered data to New List
+    val companyAfter1994List = companyList.filter { company ->
+        company.startYear > 1994
+    }
+
+    // print the list
+    println("------------------------------- Filter Company List After 1994 Using Filter Predicate ---------------------------")
+    printListUsingForEachLoop(companyAfter1994List as MutableList<Company>)
 }
